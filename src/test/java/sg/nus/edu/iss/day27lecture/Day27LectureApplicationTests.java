798d68d53ca53333ad7b22fd4c8072e5274afd35
JD-KE <60773497+JD-KE@users.noreply.github.com>
2024-01-18 09:25:45 +0800

    First Commit

---
+++ src/test/java/sg/nus/edu/iss/day27lecture/Day27LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.day27lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day27LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
